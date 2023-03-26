20d25e71d37cac5be03ac352da4adbbc7e560949
NB <nb9061@yandex.ru>
2023-03-26 16:55:22 +0500

    Progress

---
+++ File downloader/Converters/CredentialMultipurposeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace File_downloader.Converters
+{
+    class CredentialMultipurposeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isActive = (bool)value;
+            return isActive
+                ? Application.Current.Resources["MainBorderBrush"]
+                : Application.Current.Resources["UnavaibleElementBrush"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new object();
+        }
+    }
+}
