a464edc057a5d583856e7d0876014aac90dfae73
NB <nb9061@yandex.ru>
2023-03-30 01:13:14 +0500

    Interface change, part of a notification logic

--- File downloader/ViewModels/MainWindowViewModel.cs
+++ File downloader/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using File_downloader.Command;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace File_downloader.ViewModels
 {
-    class MainWindowViewModel
+    // First tab
+    partial class MainWindowViewModel
     {
+
+    }
+
+    // Second tab
+    partial class MainWindowViewModel
+    {
+
+    }
+
+    // Notification panel
+    partial class MainWindowViewModel
+    {
+        private readonly AutoEventCommandBase closeNotificatonCommand;
+        private readonly AutoEventCommandBase closeAllNotificatonsCommand;
+
+        public AutoEventCommandBase CloseNotificatonCommand => closeNotificatonCommand;
+        public AutoEventCommandBase CloseAllNotificationsCommand => closeAllNotificatonsCommand;
+        public ObservableCollection<Notification> Notifications { get; set; }
+
+        private void RemoveNotification(object o)
+        {
+            var notification = (Notification)o;
+            Notifications.Remove(notification);
+        }
+
+        private void ClearNotifications()
+        {
+            Notifications.Clear();
+        }
+
+        public void AddPositiveNotification(string message)
+        {
+            Notifications.Add(new Notification
+            {
+                ImageUri = "",
+                Message = message,
+            });
+        }
+
+        public void AddNegativeNotification(Exception exception)
+        {
+            Notifications.Add(new Notification
+            {
+                ImageUri = "",
+                Message= exception.Message,
+            });
+        }
     }
 }
---
+++ File downloader/ViewModels/Notification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_downloader.ViewModels
+{
+    class Notification
+    {
+        public string ImageUri { get; set; } = "";
+        public string Message { get; set; } = "";
+    }
+}
